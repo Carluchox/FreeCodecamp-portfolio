43e36eaee00b2b8a88e2b60be6f3512754336e35
Carluchox <creges23@gmail.com>
2022-12-28 18:08:31 -0300

    Agregué un ícono de Whatsapp

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,3 +18,14 @@ navLink.forEach((link) =>
     ul.classList.remove("show");
   })
 );
+
+// scroll to top functionality
+const scrollUp = document.querySelector("#scroll-up");
+
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
